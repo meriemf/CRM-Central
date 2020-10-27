34c5db9554db2f7c3330e72103b900e06d1b3fbe
meriemf <farsi.meriem60@gmail.com>
2020-10-27 19:57:30 -0400

    replace with ishmeet code

--- project/client/src/components/ClientItem.js
+++ project/client/src/components/ClientItem.js
@@ -1,64 +1,103 @@
 
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom'; 
-//import ProjectItem from './ProjectItem';
-import ClientProjectItem from './ClientProjectItem'; 
-import axios from 'axios';
+// import React, { useState, useEffect } from 'react';
+// import { Link } from 'react-router-dom'; 
+// //import ProjectItem from './ProjectItem';
+// import ClientProjectItem from './ClientProjectItem'; 
+// import axios from 'axios';
 
-const ClientItem = ({id, first_name, last_name, email, region, tweeter_username, onDelete}) => {
-const [isOpen, setIsOpen] = useState(false);
-const [projects, setProjects] = useState([]);
-useEffect(() => {
-  Promise.all([
-    axios.get(`/clients/${id}/projects`),
-  ]).then((all) => {
-    console.log(all);
-    setProjects(all[0].data);   
- });
-}, []);
-console.log("projects", projects);
-const clientProjects = projects.map((project) => <ClientProjectItem
-    key = {project.id}
-    id = {project.id}
-    name = {project.name}
-    start_date = {project.start_date}
-    end_date = {project.end_date}
-    type = {project.type}
-    />
-)
+// const ClientItem = ({id, first_name, last_name, email, region, tweeter_username, onDelete}) => {
+// const [isOpen, setIsOpen] = useState(false);
+// const [projects, setProjects] = useState([]);
+// useEffect(() => {
+//   Promise.all([
+//     axios.get(`/clients/${id}/projects`),
+//   ]).then((all) => {
+//     console.log(all);
+//     setProjects(all[0].data);   
+//  });
+// }, []);
+// console.log("projects", projects);
+// const clientProjects = projects.map((project) => <ClientProjectItem
+//     key = {project.id}
+//     id = {project.id}
+//     name = {project.name}
+//     start_date = {project.start_date}
+//     end_date = {project.end_date}
+//     type = {project.type}
+//     />
+// )
+// return (
+// <div className="collapsible"> 
+// <tr className="toggle" onClick ={()=> setIsOpen(!isOpen)} >  
+//     <th scope="row">{id}</th>
+//     <td>{first_name}</td>
+//     <td>{last_name}</td>
+//     <td>{email}</td>
+//     <td>{region}</td>
+//     <td>{tweeter_username}</td>
+//     {/* <th>Edit | Delete</th> */}
+//     <td> 
+//         <Link to= {`/clients/${id}/edit`}
+        
+//             className="btn btn-success mr-1"
+//             role="button"
+//             aria-pressed="true" 
+//             > Edit
+//         </Link>
+//         <button
+//             className="btn btn-success mr-1"
+//             type="button"
+//             onClick={()=>{if (window.confirm('Are you sure you wish to delete this client?')) onDelete(id)}}> Delete
+//         </button>
+//     </td>
+// {isOpen && 
+// <div>
+//         <ul>
+//              {clientProjects}
+//         </ul>
+// </div>}
+// </tr>
+// </div>
+
+// );
+  
+// }
+
+
+//export default ClientItem;
+import React from 'react';
+import { Link } from 'react-router-dom'; 
+
+const ClientItem = ({id, first_name, last_name, email,department,client_type,work_type, region,position_title, tweeter_username,initial_contact_made, onDelete}) => {
 return (
-<div className="collapsible"> 
-<tr className="toggle" onClick ={()=> setIsOpen(!isOpen)} >  
+<tr> 
     <th scope="row">{id}</th>
     <td>{first_name}</td>
     <td>{last_name}</td>
     <td>{email}</td>
+    <td>{department}</td>
+    <td>{client_type}</td>
+    <td>{work_type}</td>
     <td>{region}</td>
+    <td>{position_title}</td>
     <td>{tweeter_username}</td>
+    <td>{initial_contact_made}</td>
     {/* <th>Edit | Delete</th> */}
     <td> 
         <Link to= {`/clients/${id}/edit`}
         
-            className="btn btn-success mr-1"
-            role="button"
-            aria-pressed="true" 
-            > Edit
+         className="btn btn-success mr-1"
+         role="button"
+         aria-pressed="true" 
+        > Edit
         </Link>
         <button
-            className="btn btn-success mr-1"
-            type="button"
-            onClick={()=>{if (window.confirm('Are you sure you wish to delete this client?')) onDelete(id)}}> Delete
-        </button>
-    </td>
-{isOpen && 
-<div>
-        <ul>
-             {clientProjects}
-        </ul>
-</div>}
-</tr>
-</div>
+        className="btn btn-success mr-1"
+        type="button"
+        onClick={()=>{ onDelete(id)}}> Delete
+        </button></td>
 
+</tr>
 );
   
 }
--- project/client/src/components/ClientList.js
+++ project/client/src/components/ClientList.js
@@ -1,3 +1,77 @@
+// import React, { useState, useEffect } from 'react';
+// import axios from 'axios';
+// import ClientItem from './ClientItem';
+
+// const ClientList = () => {
+
+// //set default value of client to array
+  
+// const [clients, setClients] = useState([]);
+// useEffect(() => {
+//   Promise.all([
+//     axios.get('/clients'),
+//   ]).then((all) => {
+//     setClients(all[0].data);   
+//  });
+// }, []);
+
+// const getClients = async()=>{
+//  const response = await axios.get(`/clients`);
+//  console.log("console log of getClients", response); 
+//  setClients(response.data);
+// }
+// const handleDelete =(id) => {
+  
+//   axios.delete(`/clients/${id}`)
+//   .then( res=> {
+//     console.log("res", res);
+//     if(res.status !== 200) {
+//       alert("Not able to delete client");
+//     }
+//     else {
+//       alert("client deleted");
+//       getClients();
+//     }
+//   })
+// }
+
+// const clients_list = clients.map((client) => <ClientItem
+//   key = {client.id}
+//   id = {client.id}
+//   first_name = {client.first_name}
+//   last_name = {client.last_name}
+//   email = {client.email}
+//   region = {client.region}
+//   tweeter_username = {client.tweeter_username}
+//   onDelete={handleDelete}
+//   />
+//  )
+
+
+// return (
+//   <table className="table table-striped">
+//   <thead>
+//     <tr>
+//       <th scope="col">id</th>
+//       <th scope="col">First Name</th>
+//       <th scope="col">Last Name</th>
+//       <th scope="col">Email</th>
+//       <th scope="col">Region</th>
+//       <th scope="col">Tweeter_Username</th>
+//       <th scope="col">Actions</th>
+//     </tr>
+//   </thead>
+  
+//   <tbody>
+//      {clients_list}
+//   </tbody>
+// </table>
+// );
+
+// }
+
+
+// export default ClientList;
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import ClientItem from './ClientItem';
@@ -21,16 +95,16 @@ const getClients = async()=>{
  setClients(response.data);
 }
 const handleDelete =(id) => {
-  
-  axios.delete(`/clients/${id}`)
+  axios.put(`/clients/${id}`)
   .then( res=> {
     console.log("res", res);
     if(res.status !== 200) {
       alert("Not able to delete client");
     }
     else {
-      alert("client deleted");
       getClients();
+      alert("client deleted");
+      
     }
   })
 }
@@ -41,8 +115,13 @@ const clients_list = clients.map((client) => <ClientItem
   first_name = {client.first_name}
   last_name = {client.last_name}
   email = {client.email}
+  department = {client.department}
+  client_type ={client.client_type}
+  work_type ={client.work_type}
   region = {client.region}
+  position_title = {client.position_title}
   tweeter_username = {client.tweeter_username}
+  initial_contact_made ={client.initial_contact_made}
   onDelete={handleDelete}
   />
  )
@@ -56,8 +135,13 @@ return (
       <th scope="col">First Name</th>
       <th scope="col">Last Name</th>
       <th scope="col">Email</th>
+      <th scope="col">Department</th>
+      <th scope="col">Client Type</th>
+      <th scope="col">Work Category</th>
       <th scope="col">Region</th>
+      <th scope="col">Position Title</th>
       <th scope="col">Tweeter_Username</th>
+      <th scope="col">Initial Contact Done</th>
       <th scope="col">Actions</th>
     </tr>
   </thead>
